7001516b67f01254f0cd232010dfb431d7ce6d82
rronig <gashi.1rron@gmail.com>
2025-05-03 15:25:40 +0200

    Update innolearn_db.sql

--- innolearn_db.sql
+++ innolearn_db.sql
@@ -167,3 +167,6 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Create the first super admin account
+INSERT INTO admins (username, password, role, is_active) 
+VALUES ('superadmin', 'admin123', 'super_admin', 1); 
